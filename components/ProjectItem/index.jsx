72807d0b2203d94f24fe0039bcc5af353d0b00d9
Sebastián Capelli <scapelli@fi.uba.ar>
2026-02-16 11:08:53 -0300

    remove warnings

--- components/ProjectItem/index.jsx
+++ components/ProjectItem/index.jsx
@@ -1,26 +1,28 @@
+// components/ProjectItem/index.jsx
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const renderSkills = (skills) => {
   return (
-  <div className="grid grid-cols-2 gap-4">
-  {skills.map((skill) => (
-    <Image
-      src={`/skills/${skill}.png`}
-      width="64"
-      height="64"
-      alt="/"
-    />
-  ))}
-  </div>)
+    <div className="grid grid-cols-2 gap-4">
+      {skills.map((skill) => (
+        <Image
+          key={skill}
+          src={`/skills/${skill}.png`}
+          width={64}
+          height={64}
+          alt={skill}
+        />
+      ))}
+    </div>)
 }
 
-const index = ({ title, thumbnail, projectURL, skills}) => {
+const index = ({ title, thumbnail, projectURL, skills }) => {
   return (
     <Link href={projectURL} >
       <div className="relative p-2 space-x-8 flex flex-col md:flex-row items-center justify-center md:flex shadow-custom-large shadow-gray-400  justify-center  shadow rounded-xl group hover:scale-105 ease-in duration-200">
-      <Image
+        <Image
           className="rounded-xl w-full md:w-auto"
           src={thumbnail}
           loading="lazy"
